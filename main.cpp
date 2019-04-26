949dbca8de7310f25feb7bfa3abd732f0c2c12a6
brianChen0000 <50001902+brianChen0000@users.noreply.github.com>
2019-04-26 08:53:58 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,36 @@
+//
+//  main.cpp
+//  homework9
+//
+//  Created by Mac on 2019/3/22.
+//  Copyright © 2019年 Mac. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+int main(int argc,char** argv)
+{
+    int a,b;
+    cout<<"1為男生,2為女生,請輸入你的性別與年齡"<<endl;
+    cout<<"性別為：";
+    cin>>a;
+    cout<<"年齡為：";
+    cin>>b;
+    if(a==1;b>=18)
+    {
+        cout<<"可以結婚"<<endl;
+    }
+    else if(a==1;b<18)
+        {
+            cout<<"不能結婚"<<endl;
+        }
+    else if(a==2;b>=16)
+    {
+        cout<<"可以結婚"<<endl;
+    }
+    else if(a==2;b<16)
+    {
+        cout<<"不能結婚"<<endl;
+    }
+    return 0;
+}
